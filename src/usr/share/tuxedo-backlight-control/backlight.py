9b01bcdd6a12eee04cee0445b00f02c82ea460bf
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2019-12-02 22:11:16 +0100

    Pylint backlight.py

--- src/usr/share/tuxedo-backlight-control/backlight.py
+++ src/usr/share/tuxedo-backlight-control/backlight.py
@@ -1,16 +1,19 @@
 #!/usr/bin/pkexec /usr/bin/python3
 
 import os
-from sys import argv
-from colors import colors
-from backlight_control import *
 import subprocess
+from sys import argv
+
+from backlight_control import BacklightControl, backlight
 
 sd = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == '__main__':
     if len(argv) == 1:
-        exit('Tuxedo Backlight Ctrl: No command specified. Try "backlight --help" to list available commands')
+        exit((
+          "Tuxedo Backlight Ctrl: No command specified."
+          "Try 'backlight --help' to list available commands"
+        ))
 
     cmd = argv[1]
     if cmd == '--help' or cmd == '-h':
@@ -19,18 +22,24 @@ if __name__ == '__main__':
 
     if cmd == '--version' or cmd == '-v':
         exit(BacklightControl.VERSION)
-            
+
     if not os.path.isdir(BacklightControl.DEVICE_PATH):
-        exit('Tuxedo Backlight Ctrl: The tuxedo_keyboard module is not installed at ' + BacklightControl.DEVICE_PATH)
+        exit((
+          "Tuxedo Backlight Ctrl: The tuxedo_keyboard module is "
+          "not installed at " + BacklightControl.DEVICE_PATH
+        ))
+
+    colorlist = list(BacklightControl.colors.keys())
 
     if cmd == 'ui':
-        subprocess.call(['/usr/share/tuxedo-backlight-control/ui.py'])
+        from ui import init
+        init()
     elif cmd == 'off':
         backlight.state = 0
     elif len(argv) == 2 and cmd in backlight.modes:
         backlight.state = 1
         backlight.mode = cmd
-    elif len(argv) == 3 and cmd == 'color' and argv[2] in list(BacklightControl.colors.keys()):
+    elif len(argv) == 3 and cmd == 'color' and argv[2] in colorlist:
         backlight.state = 1
         backlight.set_single_color(argv[2])
     elif len(argv) == 6:
