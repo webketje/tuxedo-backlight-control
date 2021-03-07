18ecaac629c2d6530ab434d889c53754a5b2e425
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2021-03-07 23:04:17 +0100

    feat: Refactor CLI to use python argparse

--- src/usr/share/tuxedo-backlight-control/backlight.py
+++ src/usr/share/tuxedo-backlight-control/backlight.py
@@ -3,73 +3,62 @@
 import os
 import subprocess
 from sys import argv
-
+import argparse
 from backlight_control import BacklightControl, backlight
 
-sd = os.path.dirname(os.path.realpath(__file__))
+def exec_ui(args):
+    from ui import init
+    init()
+    exit()
 
+def set_color(args):
+    if len(args.colors) == 1:
+        BacklightControl.set_single_color(args.values[0])
+    else:
+        for region in BacklightControl.regions:
+            index = BacklightControl.regions.index(region)
+            if len(args.values) > index:
+                mapped_color = BacklightControl.find_color_by_key(args.values[index])
+                BacklightControl.set_device_param('color_' + region, mapped_color)
 
-def error_invalid_brightness(arg: str):
-    return f"Error: Invalid argument '{arg}' provided. Please provide a number from 0 - 255 to set brightness"
+def set_mode(args):
+    BacklightControl.mode = args.value
+def set_off(args):
+    BacklightControl.state = '0'
+def set_brightness(args):
+    BacklightControl.brightness = args.value
 
-def error_missing_cmd():
-    return "Error: No command specified. Try 'backlight --help' to list available commands."
+colorlist = BacklightControl.colors.keys()
+parser = argparse.ArgumentParser(prog='backlight', description='<tuxedo-backlight-control> - v0.7 - 2020-08-14, by Kevin Van Lierde <kevin.van.lierde@gmail.com>')
+subparsers = parser.add_subparsers()
 
-def error_missing_arg(cmd: str):
-    return f"Error: No argument specified for '{cmd}'. Try 'backlight --help' to list available comamnds and valid arguments."
+colorparser = subparsers.add_parser('color',
+    help="""
+        Set the keyboard backlight to a single color.
+        Alternatively, set the keyboard to 1-4 distinct colors, in the order: left, center, right, extra.
+        Available colors are: {colors} .
+        Only regions supported by your keyboard will have effect.
+    """.format(colors=', '.join([str(x) for x in BacklightControl.colors.keys()]))
+)
+colorparser.add_argument('values', choices=colorlist, nargs='+')
+colorparser.set_defaults(action=set_color)
 
-def error_no_driver():
-    return f"Error: The tuxedo_keyboard module is not installed at {BacklightControl.DEVICE_PATH}"
+modeparser = subparsers.add_parser('mode', help='Set the keyboard backlight to <mode>, one of: breathe, cycle, dance, flash, random, tempo, wave')
+modeparser.add_argument('value', choices=BacklightControl.modes, nargs=1, help='Set the keyboard backlight to <mode>')
+modeparser.set_defaults(action=set_mode)
 
+brightness_args = subparsers.add_parser('brightness', help='Set keyboard backlight brightness from 0-255')
+brightness_args.add_argument('value', type=int)
+brightness_args.set_defaults(action=set_brightness)
 
-if __name__ == "__main__":
-    if len(argv) == 1:
-        exit(error_missing_cmd())
+stateparser = subparsers.add_parser('off', help='Turn off keyboard backlight')
+stateparser.set_defaults(action=set_off)
 
-    cmd = argv[1]
-    if cmd == "--help" or cmd == "-h":
-        with open(sd + "/help.txt", "r") as fin:
-            exit(fin.read())
+uiparser = subparsers.add_parser('ui', help='Start the TUXEDO Backlight Control UI')
+uiparser.set_defaults(action=exec_ui)
 
-    if cmd == "--version" or cmd == "-v":
-        exit(BacklightControl.VERSION)
+parser.add_argument('-v', '--version', action='version', version='0.7', help='Display app version')
+parsed = parser.parse_args(argv[1:])
 
-    if not os.path.isdir(BacklightControl.DEVICE_PATH):
-        exit(error_no_driver())
-
-    colorlist = list(BacklightControl.colors.keys())
-
-    if cmd == "ui":
-        from ui import init
-        init()
-        exit()
-
-    if cmd == "off":
-        backlight.state = 0
-        exit()
-
-    if len(argv) == 2 and cmd in backlight.modes:
-        backlight.state = 1
-        backlight.mode = cmd
-
-    if len(argv) < 3:
-        exit(error_missing_arg(cmd))
-
-    arg = argv[2]
-    if cmd == "brightness":
-        backlight.state = 1
-        try:
-            arg = int(arg)
-        except ValueError as error:
-            exit(error_invalid_brightness(arg))
-        if arg > 255 or arg < 0:
-            exit(error_invalid_brightness(arg))
-
-        backlight.brightness = arg
-    elif len(argv) == 3 and cmd == "color" and arg in colorlist:
-        backlight.state = 1
-        backlight.set_single_color(arg)
-    elif len(argv) == 6:
-        backlight.state = 1
-        for index, region in enumerate(backlight.regions):
-            setattr(backlight, "color_" + region, argv[2 + index])
+if 'action' in vars(parsed):
+    parsed.action(parsed)
